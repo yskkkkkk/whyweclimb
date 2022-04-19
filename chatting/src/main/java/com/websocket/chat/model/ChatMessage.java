10fe44958fb3da8036ae67eed8e47c72cb74898f
kwwoo516 <kwwoo516@naver.com>
2022-04-19 18:11:46 +0900

    test: mattermost conn

--- chatting/src/main/java/com/websocket/chat/model/ChatMessage.java
+++ chatting/src/main/java/com/websocket/chat/model/ChatMessage.java
@@ -6,12 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ChatMessage {
-    public enum MessageType{
+    public enum MessageType {
         ENTER, TALK
     }
+
     private MessageType type;
     private String roomId;
     private String sender;
     private String message;
-    
+
 }
