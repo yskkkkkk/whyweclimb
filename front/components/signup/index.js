a212194c9969ea2b2828d5b8623d558915edc557
KIM Tae Hun <gift2barry@gmail.com>
2022-04-22 11:50:10 +0900

    feat: Add id confirmation logic

--- front/components/signup/index.js
+++ front/components/signup/index.js
@@ -1,7 +1,57 @@
 import style from './signup.module.css';
+import { useState } from 'react';
+
+const ID_REGEX = /^[a-zA-z][a-zA-Z0-9]{3,20}$/;
+const PW_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$*]).{8,24}$/;
 
 export default function Signup ({toLogin}) {
   
+  const [userId, setUserId] = useState("");
+  const [validUserId, setValidUserId] = useState(false);
+  const [availableUserId, setAvailableUserId] = useState(false);
+
+  const [userPassword, setUserPassword] = useState("");
+  const [validUserPassword, setValidUserPassword] = useState(false);
+
+  const [matchPassword, setMatchPassword] = useState("");
+  const [validMatchPassword, setValidMatchPassword] = useState(false);
+
+  const [formReady, setFormReady] = useState(false);
+
+  
+  const initializeData = () => {      // 회원가입 누르면 회원 정보 입력창 정보 초기화
+    setUserId("");
+    setUserPassword("");
+    setMatchPassword("");
+    setValidUserId(false);
+    setAvailableUserId(false);
+    setValidUserPassword(false);
+    setValidMatchPassword(false);
+    setFormReady(false);
+  };
+
+  const userIdCheck = (e) => {         // 중복되는 아이디 없는지 확인
+    e.preventDefault();
+    if (validUserId && userId) {
+      fetch(/**/)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data) {
+            setAvailableUserId(data);
+            alert('유효한 아이디입니다');
+          }
+          else {
+            alert('이미 사용중인 아이디입니다');
+          }
+        });
+    }
+    else {
+      alert('유효한 아이디를 입력해주세요');
+    }
+  }
+ 
+  
+
   return (
     <main className={style.container}>
       <h2>
@@ -9,7 +59,10 @@ export default function Signup ({toLogin}) {
       </h2>
       <section className={style.login}>
         <div className={style.card}>
-          <label>Id <input type="text" /></label>
+          <label>Id 
+            <input type="text" />
+            <button onClick={userIdCheck}>중복확인</button>
+          </label>
         </div>
         <div className={style.card}>
           <label>Password <input type="password" /></label>
