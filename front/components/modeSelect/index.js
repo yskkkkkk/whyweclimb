b9fa217a345da723eea06fc270d0070e9f68d245
KIM Tae Hun <gift2barry@gmail.com>
2022-04-22 23:41:07 +0900

    design: Add skeleton assets for single mode game

--- front/components/modeSelect/index.js
+++ front/components/modeSelect/index.js
@@ -6,7 +6,7 @@ export default function ModeSelect({toMain, toConfigure}) {
   
   return (
     <main className={style.container}>
-      <Link href={'/game'} passHref>
+      <Link href={'/singleGame'} passHref>
         <h2>single mode</h2>
       </Link>
       <h2>multi mode</h2>
