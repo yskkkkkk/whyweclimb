c52b7d8179e71fc68b94fffbd902b6539be9f18e
kwwoo516 <kwwoo516@naver.com>
2022-04-25 14:50:06 +0900

    chore: Add setting code

---
+++ backend/src/test/java/com/whyweclimb/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whyweclimb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
