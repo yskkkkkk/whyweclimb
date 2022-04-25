b55b44479224b6320d802634269bc5b7122e12aa
kwwoo516 <kwwoo516@naver.com>
2022-04-25 14:46:49 +0900

    rename : project name change

---
+++ backend/src/main/java/com/whyweclimb/backend/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.whyweclimb.backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    public enum MessageType {
+        ENTER, TALK
+    }
+
+    private MessageType type;
+    private String roomId;
+    private String sender;
+    private String message;
+}
