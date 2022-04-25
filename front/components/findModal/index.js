c4295a4b9733e534538f2280c9b02ef7e06f9c19
KIM Tae Hun <gift2barry@gmail.com>
2022-04-25 20:24:44 +0900

    feat: Add multi room find component logic

--- front/components/findModal/index.js
+++ front/components/findModal/index.js
@@ -1,14 +1,40 @@
+import {useRouter} from 'next/router';
+import { useState, useEffect } from 'react';
 import style from './findModal.module.css';
 
 export default function FindModal({toggleFindModal}) {
+
+  const router = useRouter();
+
+  const [roomID, setRoomID] = useState('');
+
+  const writeRoomID = (e) => {
+    setRoomID(e.target.value);
+  };
+
+  // const findRoom = () => {
+  //   fetch(`https://${userId}/exists`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (data) {
+  //         router.push(`/${roomID}`);
+  //       }
+  //       else {
+  //         alert('invalid RoomID');
+  //       }
+  //     });
+  // };
 
   return (
     <section className={style.modal}>
       <div className={style.card}>
         <label>search room 
-          <input type="text" required /></label>
+          <input type="text" required onChange={writeRoomID} /></label>
       </div>
-      <button onClick={toggleFindModal} >close</button>
+      <div className={style.btns}>
+        <button /*onClick={findRoom}*/ >join</button>
+        <button onClick={toggleFindModal} >close</button>
+      </div>
     </section>
   )
 }
