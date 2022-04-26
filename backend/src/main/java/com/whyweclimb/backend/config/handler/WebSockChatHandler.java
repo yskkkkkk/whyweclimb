4c52726495318f27df9ec39f0581091538a3d4ba
kwwoo516 <kwwoo516@naver.com>
2022-04-26 17:50:02 +0900

    test: Build test

--- backend/src/main/java/com/whyweclimb/backend/config/handler/WebSockChatHandler.java
+++ backend/src/main/java/com/whyweclimb/backend/config/handler/WebSockChatHandler.java
@@ -13,15 +13,4 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class WebSockChatHandler extends TextWebSocketHandler {
     private final ObjectMapper objectMapper;
     private final ChatService chatService;
-
-//    @Override
-//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
-//        String payload = message.getPayload();
-//        log.info("payload {}",payload);
-////        TextMessage textMessage = new TextMessage("Welcome chatting server");
-////        session.sendMessage(textMessage);
-//        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
-//        ChatRoom room.ftl = chatService.findRoomById(chatMessage.getRoomId());
-//        room.ftl.handleActions(session, chatMessage, chatService);
-//    }
 }
