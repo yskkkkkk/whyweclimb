140ff5e985bf22430de0a4cbf967a18cdb3e5b0e
fbeotjd <fb1014@naver.com>
2022-04-29 10:12:59 +0900

    test: jwt login

--- backend/src/main/java/com/whyweclimb/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/whyweclimb/backend/config/SwaggerConfig.java
@@ -17,7 +17,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
-// http://localhost:8081/swagger-ui/index.html#/
+// http://localhost:8081/api/swagger-ui/index.html#/
+// http://k6a401.p.ssafy.io:8081/api/swagger-ui/index.html#/
 
 @Configuration
 public class SwaggerConfig {
