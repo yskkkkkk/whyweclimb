57f3d2c336f3fdd221ab1fd14fb2c55546e9d8c6
KIM Tae Hun <gift2barry@gmail.com>
2022-04-29 17:32:44 +0900

    fix: Api request uri from http to https

--- front/components/login/index.js
+++ front/components/login/index.js
@@ -14,7 +14,7 @@ export default function Login({toSignup, toModeSelect}) {
   };
 
   const handleLoginSubmit = () => {     // 로그인 버튼 누를시 post요청 (백에서 실패사유 알려주면 땡큐)
-    fetch('http://k6a401.p.ssafy.io:8081/api/user/login', {
+    fetch('https://k6a401.p.ssafy.io/api/user/login', {
       method: "POST",
       body: JSON.stringify({
         userId: userID,
