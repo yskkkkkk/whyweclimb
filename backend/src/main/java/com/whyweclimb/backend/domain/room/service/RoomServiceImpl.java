a9c15dba8b7db0088ab4bfab54c57f7df4f53ca2
fbeotjd <fb1014@naver.com>
2022-05-12 16:27:02 +0900

    fix: find room

--- backend/src/main/java/com/whyweclimb/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/whyweclimb/backend/domain/room/service/RoomServiceImpl.java
@@ -1,6 +1,7 @@
 package com.whyweclimb.backend.domain.room.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -29,12 +30,16 @@ public class RoomServiceImpl implements RoomService {
 
 	@Override
 	public RoomInfoResponse findRoom(String roomCode) {
-		RoomInfoResponse response = roomRepository.findByRoomCode(roomCode).orElse(null);
-
-		int now = accessRedisRepository.findByRoomCode(roomCode).size();
-		int max = response.getRoomMaxNum();
+		Optional<RoomInfoResponse> RoomInfo = roomRepository.findByRoomCode(roomCode);
+		RoomInfoResponse response = null;
 		
-		if (now >= max) response = null; 
+		if(RoomInfo.isPresent()) {
+			int now = accessRedisRepository.findByRoomCode(roomCode).size();
+			int max = RoomInfo.get().getRoomMaxNum();
+			if (now < max) {
+				response = RoomInfo.get();
+			}
+		}
 		
 		return response;
 	}
