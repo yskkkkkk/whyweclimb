4b29b6f2f98cc3da27579a8628656738bb3e7b9d
정찬 <cjung0418@naver.com>
2022-05-13 16:32:28 +0900

    fix: alert messages

--- front/components/multi/findModal/index.js
+++ front/components/multi/findModal/index.js
@@ -6,6 +6,7 @@ import toast from 'react-hot-toast';
 
 export default function FindModal() {
   const basicURL = 'https://k6a401.p.ssafy.io/api'
+  // const basicURL = `http://localhost:8081/api`
   const [roomID, setRoomID] = useState('');
 
   const writeRoomID = (e) => {
@@ -17,10 +18,23 @@ export default function FindModal() {
       .then(res=>res.data)
       .then(data=>{
         if(data !== ""){
-          location.href=`/multi/${data.roomCode}`;
+          switch(data.roomFindResult) {
+            case 'ok':
+              location.href=`/multi/${data.roomCode}`;
+              break;
+            case 'full':
+              alert('방의 인원이 다 찼습니다.');
+              break;
+            case 'start':
+              alert('해당 방의 게임이 시작되었습니다.');
+              break;
+            default:
+              alert("해당하는 방이 존재하지 않습니다. 다시 한번 확인해 주세요.");
+          }
         } else {
           alert("해당하는 방이 존재하지 않습니다. 다시 한번 확인해 주세요.");
         }
+        console.log(data);
       })
       .catch(err=>console.error(err))
   }
